7ab423e49ab004d90e2fc92cf5916032940bc3f6
srirampeddamma <psriram669@gmail.com>
2025-07-23 11:51:46 +0530

    Commiting index and code

---
+++ code.js
@@ -0,0 +1,9 @@
+// Change navbar style on scroll
+window.addEventListener("scroll", () => {
+  const nav = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
